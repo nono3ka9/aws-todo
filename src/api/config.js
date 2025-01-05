dd1f77da2362bd99bac7c2e0532176ae9ac8991b
noa_k <noa.work32@gmail.com>
2025-01-05 16:59:22 +0900

    GetAll作成

---
+++ src/api/config.js
@@ -0,0 +1,6 @@
+export const ApiUrls = {
+    getTodo:
+        "https://h5t7jccjn3.execute-api.ap-northeast-1.amazonaws.com/default/GetTodo",
+    manageTodo:
+        "https://03zrugh17g.execute-api.ap-northeast-1.amazonaws.com/default/ManageTodo",
+};
--- src/api/todo_service.js
+++ src/api/todo_service.js
@@ -31,4 +31,63 @@ export class TodoService {
             is_deleted: formData.is_deleted,
         };
     }
+
+    static async fetchFromApi(url, options = {}){
+        Message.dispose();
+
+        return fetch(url, options)
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error(`HTTP error! Status: ${res.status}`);
+                }
+                return res.json();
+            })
+            .catch((error) => {
+                console.error("API error:", error);
+                throw error;
+            });
+    }
+    static async getAll() {
+        return this.fetchFromApi(ApiUrls.getTodo)
+            .then((data) =>
+                data.map(
+                    (item) =>
+                        new Todo(
+                            item.id,
+                            item.title,
+                            item.detail,
+                            item.deadLine,
+                            item.is_done,
+                            item.is_deleted
+                        )
+                )
+            )
+            .catch((error) => {
+                console.error("Error fetching todos:", error);
+                return [];
+            });
+            
+    }
+    // static async update(formData) {
+    //     const data = {
+    //         post_type: formData.post_type,
+    //         id: formData.id,
+    //         title: formData.title,
+    //         detail: formData.detail,
+    //         deadLine: formData.deadLine,
+    //         is_done: formData.is_done,
+    //         is_deleted: formData.is_deleted
+    //     };
+
+    //     return this.fetchFromApi(ApiUrls.manageTodo, {
+    //         method: "POST",
+    //         headers: { "Content-Type": "application/json"},
+    //         body: JSON.stringify(data),
+    //     })
+    //         .then(() => true)
+    //         .catch((error) => {
+    //             console.error("Error updating todo:", error);
+    //             return false;
+    //         });
+    // }
 }
