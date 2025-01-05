d2a630f204dd32c0431d3fa6d281abb4737d3d73
noa_k <noa.work32@gmail.com>
2025-01-05 17:19:15 +0900

    変更

--- src/api/todo_service.js
+++ src/api/todo_service.js
@@ -68,26 +68,26 @@ export class TodoService {
             });
             
     }
-    // static async update(formData) {
-    //     const data = {
-    //         post_type: formData.post_type,
-    //         id: formData.id,
-    //         title: formData.title,
-    //         detail: formData.detail,
-    //         deadLine: formData.deadLine,
-    //         is_done: formData.is_done,
-    //         is_deleted: formData.is_deleted
-    //     };
+    static async update(formData) {
+        const data = {
+            post_type: formData.post_type,
+            id: formData.id,
+            title: formData.title,
+            detail: formData.detail,
+            deadLine: formData.deadLine,
+            is_done: formData.is_done,
+            is_deleted: formData.is_deleted
+        };
 
-    //     return this.fetchFromApi(ApiUrls.manageTodo, {
-    //         method: "POST",
-    //         headers: { "Content-Type": "application/json"},
-    //         body: JSON.stringify(data),
-    //     })
-    //         .then(() => true)
-    //         .catch((error) => {
-    //             console.error("Error updating todo:", error);
-    //             return false;
-    //         });
-    // }
+        return this.fetchFromApi(ApiUrls.manageTodo, {
+            method: "POST",
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify(data),
+        })
+            .then(() => true)
+            .catch((error) => {
+                console.error("Error updating todo:", error);
+                return false;
+            });
+    }
 }
